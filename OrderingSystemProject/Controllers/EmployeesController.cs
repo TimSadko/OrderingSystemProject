ba8de583116b3cf6e3aa1257e8b4ca524135cb67
Abdullah SEN <sen-abdullah@yandex.com>
2025-05-31 00:36:29 +0200

    Create employee management system

--- OrderingSystemProject/Controllers/EmployeesController.cs
+++ OrderingSystemProject/Controllers/EmployeesController.cs
@@ -1,14 +1,12 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using OrderingSystemProject.Models;
-using OrderingSystemProject.Repositories;
 using OrderingSystemProject.Services;
 
 namespace OrderingSystemProject.Controllers;
 
 public class EmployeesController : Controller
 {
-
     private readonly IEmployeesService _employeesService;
 
     public EmployeesController(IEmployeesService employeesService)
@@ -18,24 +16,120 @@ public class EmployeesController : Controller
 
     public IActionResult Index()
     {
+        List<Employee> employees = _employeesService.GetAllEmployees();
+        return View(employees);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
         return View();
     }
     
+    [HttpPost]
+    public IActionResult Create(Employee employee)
+    {
+        try
+        {
+            _employeesService.Create(employee);
+            TempData["EmployeeOperationConfirmMessage"] = "Staff has been added!";
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception e)
+        {
+            ViewBag.ErrorMessage = $"Exception occured: {e.Message}";
+            return View(employee);
+        }
+    }
+    
+    
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var menuItem = _employeesService.GetById((int)id);
+        return View(menuItem);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Employee employee)
+    {
+        try
+        {
+            _employeesService.Update(employee);
+
+            TempData["EmployeeOperationConfirmMessage"] = "Staff has been updated!";
+
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception e)
+        {
+            ViewBag.ErrorMessage = $"Exception occured: {e.Message}";
+
+            return View(employee);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Activate(int employeeId)
+    {
+        try
+        {
+            _employeesService.Activate(employeeId);
+        
+            TempData["EmployeeOperationConfirmMessage"] = "Staff has been activated!";
+        
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception e)
+        {
+            ViewBag.ErrorMessage = $"Exception occured: {e.Message}";
+
+            return RedirectToAction(nameof(Index));
+        }
+        
+    }
+    
+    [HttpPost]
+    public IActionResult Deactivate(int employeeId)
+    {
+        try
+        {
+            _employeesService.Deactivate(employeeId);
+        
+            TempData["EmployeeOperationConfirmMessage"] = "Staff has been deactivated!";
+        
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception e)
+        {
+            ViewBag.ErrorMessage = $"Exception occured: {e.Message}";
+
+            return RedirectToAction(nameof(Index));
+        }
+        
+    }
+
     [HttpGet]
     public IActionResult Login()
     {
         // get the employee data from session
         string? employeeJson = HttpContext.Session.GetString("LoggedInEmployee");
-        if (employeeJson != null) 
+        if (employeeJson != null)
         {
             // converting the JSON string into an Employee object
             Employee? employee = JsonSerializer.Deserialize<Employee>(employeeJson);
-            if (employee != null) 
+            if (employee != null)
             {
                 // if employee exists in session, redirect based on employee type
-                return RedirectEmployee((EmployeeType)employee.EmployeeType); 
+                return RedirectEmployee((EmployeeType)employee.EmployeeType);
             }
-        } 
+        }
+
         // if no session exists, show login form
         return View();
     }
@@ -45,8 +139,9 @@ public class EmployeesController : Controller
     {
         try
         {
-            Employee employee = _employeesService.GetEmployeeByLoginCredentials(loginModel.UserName, loginModel.Password);
-        
+            Employee employee =
+                _employeesService.GetEmployeeByLoginCredentials(loginModel.UserName, loginModel.Password);
+
             // if credentials are incorrect
             if (employee == null)
             {
@@ -58,7 +153,7 @@ public class EmployeesController : Controller
                 // remember logged in employee
                 string userJson = JsonSerializer.Serialize(employee);
                 HttpContext.Session.SetString("LoggedInEmployee", userJson);
-                
+
                 // redirect Employee by Type
                 return RedirectEmployee((EmployeeType)employee.EmployeeType);
             }
@@ -69,12 +164,12 @@ public class EmployeesController : Controller
             return View(loginModel);
         }
     }
-    
+
     public IActionResult Logout()
     {
         // clear the session
         HttpContext.Session.Clear();
-    
+
         // redirect to login page
         return RedirectToAction("Login", "Employees");
     }
@@ -90,6 +185,7 @@ public class EmployeesController : Controller
             case EmployeeType.Cook:
                 return RedirectToAction("Index", "Kitchen");
         }
+
         return RedirectToAction("Index", "Home");
     }
 }
--- OrderingSystemProject/Services/IEmployeesService.cs
+++ OrderingSystemProject/Services/IEmployeesService.cs
@@ -6,4 +6,14 @@ public interface IEmployeesService
 {
     List<Employee> GetAllEmployees();
     Employee GetEmployeeByLoginCredentials(string userName, string password);
+    
+    void Create(Employee employee);
+    
+    Employee? GetById(int id);
+    void Update(Employee employee);
+    
+    void Activate(int employeeId);
+    
+    void Deactivate(int employeeId);
+
 }
