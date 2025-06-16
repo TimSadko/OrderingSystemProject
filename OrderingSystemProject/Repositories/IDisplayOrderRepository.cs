4b2279a96764964223f066137cc4f21f23019756
SpicyCarry <736115@student.inholland.nl>
2025-06-17 00:26:17 +0200

    remove unused files

--- OrderingSystemProject/Repositories/IDisplayOrderRepository.cs
+++
@@ -1,8 +0,0 @@
-using OrderingSystemProject.Models;
-
-namespace OrderingSystemProject.Repositories;
-
-public interface IDisplayOrderRepository
-{
-    DisplayOrderViewModel GetFullOrderSummary(int orderId);
-}
