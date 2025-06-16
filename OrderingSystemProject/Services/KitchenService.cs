82b6e50cc078ec6d08b0fb0894f5481d3b45c1a5
TimSadko <tim.sadko@gmail.com>
2025-06-16 20:09:44 +0200

    Kitchen: swaped from CommonRepository to Injection

--- OrderingSystemProject/Services/KitchenService.cs
+++ OrderingSystemProject/Services/KitchenService.cs
@@ -7,14 +7,18 @@ namespace OrderingSystemProject.Services
 {
 	public class KitchenService : IKitchenServices
 	{
-		public KitchenService() 
-		{
+        private IOrdersRepository _order_rep;
+        private IOrderItemsRepository _order_item_rep;
 
-		}
+        public KitchenService(IOrdersRepository _order_rep, IOrderItemsRepository _order_item_rep)
+        {
+            this._order_rep = _order_rep;
+            this._order_item_rep = _order_item_rep;
+        }
 
-		public List<KitchenOrder> GetCookOrders()
+        public List<KitchenOrder> GetCookOrders()
 		{
-			var list = CommonRepository._order_rep.GetOrdersKitchen(); // Get lists of orders for the repo
+			var list = _order_rep.GetOrdersKitchen(); // Get lists of orders for the repo
 
 			return list; 
 		}
@@ -38,57 +42,57 @@ namespace OrderingSystemProject.Services
 
 		public List<KitchenOrder> GetDoneCookOrders()
 		{
-			return CommonRepository._order_rep.GetDoneOrdersKitchen();
+			return _order_rep.GetDoneOrdersKitchen();
 		}
 
 		public void TakeItem(int _order_id, int _item_id)
 		{
-			var item = CommonRepository._order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
+			var item = _order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
 
 			if (item == null) throw new Exception("Invalid order item id"); // If it is null, throw exception
 		
 
 			if (item.ItemStatus != OrderItemStatus.NewItem) return; // If order item status is not new, return
 
-			CommonRepository._order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Preparing); // Update the status of order item in db
+			_order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Preparing); // Update the status of order item in db
 
-			CommonRepository._order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db			
+			_order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db			
 		}
 
 		public void FinishItem(int _order_id, int _item_id)
 		{
-			var item = CommonRepository._order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
+			var item = _order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
 
 			if (item == null) throw new Exception("Invalid order item id"); // If it is null, throw exception
 
 			if (item.ItemStatus != OrderItemStatus.Preparing) return; // If order item status is not new, return
 
-			CommonRepository._order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Ready); // Update the status of order item in db
+			_order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Ready); // Update the status of order item in db
 
-			var order = CommonRepository._order_rep.GetById(_order_id); // Get order from rep by id
+			var order = _order_rep.GetById(_order_id); // Get order from rep by id
 
 			SetOrderStatusToReadyIfAllReady(order); // Set status of the order to served, if all of the items are served
 		}
 
 		public void ReturnItem(int _order_id, int _item_id)
 		{
-			var item = CommonRepository._order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
+			var item = _order_item_rep.GetOrederItemById(_item_id); // Get order item from rep
 
 			if (item == null) throw new Exception("Invalid order item id"); // If it is null, throw exception
 
 
 			if (item.ItemStatus != OrderItemStatus.Preparing && item.ItemStatus != OrderItemStatus.Ready) return;
 
-			CommonRepository._order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Preparing); // Update the status of order item in db
+			_order_item_rep.UpdateOrderItemStatus(_item_id, OrderItemStatus.Preparing); // Update the status of order item in db
 
-			var order = CommonRepository._order_rep.GetById(_order_id); // Get order from rep by id
+			var order = _order_rep.GetById(_order_id); // Get order from rep by id
 
-			if(order.OrderStatus != OrderStatus.Preparing) CommonRepository._order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db	
+			if(order.OrderStatus != OrderStatus.Preparing) _order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db	
 		}
 
 		public void TakeOrder(int _order_id)
 		{
-			var order = new KitchenOrder(CommonRepository._order_rep.GetById(_order_id)); // Get order from rep by id
+			var order = new KitchenOrder(_order_rep.GetById(_order_id)); // Get order from rep by id
 
 			if (order == null) throw new Exception("Invalid order id"); // if could not find order throw exception
 
@@ -100,16 +104,16 @@ namespace OrderingSystemProject.Services
 
 				if (order.Items[i].ItemStatus == OrderItemStatus.NewItem) // if current item status is new
 				{
-					CommonRepository._order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Preparing); // Change status of the item in db
+					_order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Preparing); // Change status of the item in db
 				}
             }
 
-			CommonRepository._order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db		
+			_order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db		
 		}
 
 		public void FinishOrder(int _order_id)
 		{
-			var order = new KitchenOrder(CommonRepository._order_rep.GetById(_order_id)); // Get order from rep by id
+			var order = new KitchenOrder(_order_rep.GetById(_order_id)); // Get order from rep by id
 
 			if (order == null) throw new Exception("Invalid order id"); // if could not find order throw exception
 
@@ -121,7 +125,7 @@ namespace OrderingSystemProject.Services
 
 				if (order.Items[i].ItemStatus == OrderItemStatus.NewItem || order.Items[i].ItemStatus == OrderItemStatus.Preparing) // if current item status is new or preparing
 				{
-					CommonRepository._order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Ready); // Change status of the item in db
+					_order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Ready); // Change status of the item in db
 				}
 			}
 
@@ -130,7 +134,7 @@ namespace OrderingSystemProject.Services
 
 		public void ReturnOrder(int _order_id)
 		{
-			var order = new KitchenOrder(CommonRepository._order_rep.GetById(_order_id)); // Get order from rep by id
+			var order = new KitchenOrder(_order_rep.GetById(_order_id)); // Get order from rep by id
 
 			if (order == null) throw new Exception("Invalid order id"); // if could not find order throw exception
 
@@ -142,11 +146,11 @@ namespace OrderingSystemProject.Services
 
 				if (order.Items[i].ItemStatus == OrderItemStatus.Ready) // if current item status is new or preparing
 				{
-					CommonRepository._order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Preparing); // Change status of the item in db
+					_order_item_rep.UpdateOrderItemStatus(order.Items[i].Id, OrderItemStatus.Preparing); // Change status of the item in db
 				}
 			}
 
-			CommonRepository._order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db	
+			_order_rep.UpdateOrderStatus(_order_id, OrderStatus.Preparing); // Update the status of order in db	
 		}
 
 		private void SetOrderStatusToReadyIfAllReady(Order order)
@@ -162,7 +166,7 @@ namespace OrderingSystemProject.Services
 				}
 			}
 
-			if (all_items_ready) CommonRepository._order_rep.UpdateOrderStatus(order.OrderId, OrderStatus.ReadyForPickup); // If all of them are ready, then update the status of order in db	
+			if (all_items_ready) _order_rep.UpdateOrderStatus(order.OrderId, OrderStatus.ReadyForPickup); // If all of them are ready, then update the status of order in db	
 		}
 	}
 }
